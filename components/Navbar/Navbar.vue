fd9c05ce805e08fe338a73733f526dc5f52938f4
Pawan918 <pawanshah84489@gmail.com>
2023-12-01 01:20:05 +0530

    added demo login and logout functionality

---
+++ components/Navbar/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+   <div class="h-16 bg-black text-white flex justify-end items-center gap-4 px-6">
+    <UButton color="red" variant="solid" label="Login" size="md" v-if="!isLoggedIn" @click="loginHandler"/>
+    <NavbarProfile v-else/>
+   </div>
+</template>
+<script setup >
+const userStore = useUserStore();
+const { isLoggedIn } = storeToRefs(userStore)
+const loginHandler = ()=>{
+  isLoggedIn.value = true;
+}
+
+</script>
