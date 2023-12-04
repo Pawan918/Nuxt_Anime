ada182cd32098819680bf50029a01dfb38ce2706
Pawan918 <pawanshah84489@gmail.com>
2023-12-05 00:21:41 +0530

    Create home page trending Cards

---
+++ pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="pt-6 px-6 bg-[#1b1a1a]">
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8 ">
+            <div v-for="data in data.results" class="cursor-pointer">
+                <Card :data="data" class="h-72"/>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+const  {data}  = await useFetch('https://api-amvstrm.nyt92.eu.org/api/v2/trending');
+console.log(data)
+</script>
